8e292b1794654115abba9d707e388ddbdc9f70e0
david lobser <davidlobser@yahoo.com>
2020-09-10 13:31:18 -0600

    tooltips for laser pointer

--- Interaction/Scripts/LaserPointer.cs
+++ Interaction/Scripts/LaserPointer.cs
@@ -4,48 +4,51 @@ using UnityEngine;
 
 namespace CUVR{
 
-public class LaserPointer : MonoBehaviour {
+    public class LaserPointer : MonoBehaviour {
 
-    public RaycastHit hit;
-    public GameObject endPoint;
-    public Button button;
-    MeshRenderer[] renderers;
+        public RaycastHit hit;
+        [Tooltip("Not required: This object will appear where the laser hits")]
+        public GameObject endPoint;
+        [Tooltip("Not required: Add a button to only activate the laser when it's pressed")]
+        public Button button;
+        MeshRenderer[] renderers;
 
-	private void Start()
-	{
-        renderers = GetComponentsInChildren<MeshRenderer>();
-	}
+    	private void Start()
+    	{
+            renderers = GetComponentsInChildren<MeshRenderer>();
+    	}
 
-	void Update() {
-        if (button == null || button.click > .5f)
-        {
-            foreach (MeshRenderer m in renderers)
+    	void Update() {
+            if (button == null || button.click > .5f)
             {
-                m.enabled = true;
-            }
-            if (Physics.Raycast(transform.position, transform.forward, out hit))
-            {
-                transform.localScale = new Vector3(1, 1, Vector3.Distance(this.transform.position, hit.point));
-                if (endPoint != null)
+                foreach (MeshRenderer m in renderers)
                 {
-                    endPoint.transform.position = hit.point;
-                    endPoint.SetActive(true);
+                    m.enabled = true;
                 }
+                if (Physics.Raycast(transform.position, transform.forward, out hit))
+                {
+                    transform.localScale = new Vector3(1, 1, Vector3.Distance(this.transform.position, hit.point));
+                    if (endPoint != null)
+                    {
+                        endPoint.transform.position = hit.point;
+                        endPoint.SetActive(true);
+                    }
 
+                }
+                else
+                {
+                    if (endPoint != null)
+                        endPoint.SetActive(false);
+                    transform.localScale = new Vector3(1, 1, 100);
+                }
             }
-            else
-            {
-                endPoint.SetActive(false);
-                transform.localScale = new Vector3(1, 1, 100);
-            }
-        }
-        else{
-            foreach(MeshRenderer m in renderers){
-                m.enabled = false;
+            else{
+                foreach(MeshRenderer m in renderers){
+                    m.enabled = false;
+                }
             }
         }
     }
-}
 
 
 }
